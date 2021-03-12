66e295791f3749686e1abf6de4d0715174478735
parth-12797 <78936647+parth-12797@users.noreply.github.com>
2021-03-11 21:10:04 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,42 @@ $(document).ready(function() {
  var count=document.querySelectorAll("input:checked").length
   document.getElementById('no_seat').innerHTML = '';
 	document.getElementById('no_seat').innerHTML = count;
+var e = document.getElementById("m_name").value;
+	
+	if(e=="padmavati")
+	{
+
+p=count*13;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = p;
+	}
+	else if(e=="raaz")
+	{
+p=count*13;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = p;
+
+	}
+	else{
+p=count*7;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = p;
+
+	}	
 
 });
  
 });
 
 
-function get_movie() {
+function get_movie(divid) {
+	  var inputs = document.querySelectorAll('.check1');
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].checked = false;
+  }
+
+  
+
 
 	var e = document.getElementById("m_name").value;
 	document.getElementById('movie_name').innerHTML = '';
@@ -19,6 +48,8 @@ function get_movie() {
 document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "13$";
   document.getElementById('no_seat').innerHTML = '';
+  document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
 
 	}
 	else if(e=="raaz")
@@ -26,12 +57,16 @@ document.getElementById('price').innerHTML = '';
 document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "10$";
   document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
 
 	}
 	else{
 		document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "7$";
   document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
 
 	}
 }
