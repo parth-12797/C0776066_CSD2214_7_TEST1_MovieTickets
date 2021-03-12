4d99401ba92e13489d1f1f50706f461c428af361
parth-12797 <78936647+parth-12797@users.noreply.github.com>
2021-03-11 20:21:10 -0500

    Update index.js
    changes made

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+	$('input[type="checkbox"]').click(function () {
+ var count=document.querySelectorAll("input:checked").length
+  document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('no_seat').innerHTML = count;
+
+});
+ 
+});
+
+
 function get_movie() {
 
 	var e = document.getElementById("m_name").value;
@@ -7,14 +18,21 @@ function get_movie() {
 	{
 document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "13$";
+  document.getElementById('no_seat').innerHTML = '';
+
 	}
 	else if(e=="raaz")
 	{
 document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "10$";
+  document.getElementById('no_seat').innerHTML = '';
+
 	}
 	else{
 		document.getElementById('price').innerHTML = '';
 	document.getElementById('price').innerHTML = "7$";
+  document.getElementById('no_seat').innerHTML = '';
+
 	}
 }
+
